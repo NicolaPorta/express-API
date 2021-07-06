769802ac6f934aab26f234e0e88241dad401a203
Nicola <nicola.porta00@gmail.com>
2021-07-06 10:10:29 +0200

    Refactoring getToDoList Controller

--- controllers/toDos/getToDoList.js
+++ controllers/toDos/getToDoList.js
@@ -1,12 +1,14 @@
 const ToDo = require("../../DB/testDB");
 
 async function getToDoList(res) {
-    const toDoList = await ToDo.find();
-    const result = toDoList.map(({_id, text}) => ({
-      id: _id,
-      text
-    }));
-    res.json(result);
+    await ToDo.find()
+    .then(toDoList => {
+        const result = toDoList.map(({_id, text}) => ({
+            id: _id,
+            text
+         }));
+        res.json(result);
+    });
 };
 
 module.exports = getToDoList;
