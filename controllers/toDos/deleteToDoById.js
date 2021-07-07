01d42b6c9e366a3e733f8b695def9a3bdbd87beb
Nicola <nicola.porta00@gmail.com>
2021-07-07 15:16:38 +0200

    Refactoring deleteToDo controller

--- controllers/toDos/deleteToDoById.js
+++ controllers/toDos/deleteToDoById.js
@@ -2,7 +2,14 @@ const ToDo = require('../../DB/initializeDB');
 
 async function deleteToDoById(req, res) {
     await ToDo.findOneAndDelete({_id: req.params.id})
-    .then(todo => res.send({todo, response: `'${todo.text}' with id: ${todo._id} has been deleted`}));
+    .then(todo => {
+        if(todo.text) {
+            res.send({todo, response: `'${todo.text}' with id: ${todo._id} has been deleted`});
+        } else {
+            res.send({todo, response: `This ToDo has been deleted`});
+        }
+        
+    })
 };
 
 module.exports = deleteToDoById;
